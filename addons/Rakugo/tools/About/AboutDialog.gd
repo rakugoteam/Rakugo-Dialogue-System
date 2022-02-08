341922fe843beb603810af56a101cba3cc108ba1
Jeremi Biernacki <jeremi360@gmail.com>
2022-02-08 08:14:09 +0100

    Remove not longer used scripts

--- addons/Rakugo/tools/About/AboutDialog.gd
+++
@@ -1,9 +0,0 @@
-tool
-extends AcceptDialog
-
-func _on_WebSiteButton_pressed():
-	OS.shell_open("https://rakugoteam.github.io/")
-
-
-func _on_BugButton_pressed():
-	OS.shell_open("https://github.com/rakugoteam/Rakugo/labels/bug")
