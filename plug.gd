b333959a4b4d0de074757ecdb5424c6b7d0ca06c
Jeremi Biernacki <jeremi360@gmail.com>
2022-03-08 08:47:18 +0100

    add gd-plug with project-settings-helpers install

---
+++ plug.gd
@@ -0,0 +1,6 @@
+extends "res://addons/gd-plug/plug.gd"
+
+func _plugging():
+    # Declare your plugins in here with plug(src, args)
+    # By default, only "addons/" directory will be installed
+    plug("rakugoteam/project-settings-helpers")
