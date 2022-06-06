680639161ff8f5bf8624bd1c93b8e420232a75ec
theLudovyc <theludovyc@gmail.com>
2022-06-06 23:13:18 +0200

    Wrote TestSay

---
+++ Test/TestParser/TestSay/TestSay.gd
@@ -0,0 +1,42 @@
+extends GutTest
+
+const file_name = "res://Test/TestParser/TestSay/TestSay.rk"
+
+func before_all():
+	Rakugo.connect("say", self, "_on_say")
+	
+	Rakugo.parse_script(file_name)
+	
+var say_char:Dictionary
+var say_text:String
+func _on_say(character:Dictionary, text:String):
+	say_char = character
+	say_text = text
+
+func test_say():
+	yield(yield_to(Rakugo, "say", 0.1), YIELD)
+	
+	assert_true(say_char.empty())
+	assert_eq(say_text, "Hello, world !")
+	
+	Rakugo.do_step()
+	
+	yield(yield_to(Rakugo, "say", 0.1), YIELD)
+	
+	assert_false(say_char.empty())
+	assert_true(say_char["name"] == "Sylvie")
+	assert_eq(say_text, "Hello !")
+	
+	Rakugo.do_step()
+	
+	yield(yield_to(Rakugo, "say", 0.1), YIELD)
+	
+	assert_eq(say_text, "My name is Sylvie")
+	
+	Rakugo.do_step()
+	
+	yield(yield_to(Rakugo, "say", 0.1), YIELD)
+	
+	assert_eq(say_text, "I am 18")
+	
+	Rakugo.do_step()
