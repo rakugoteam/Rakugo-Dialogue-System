89d739adb63b3716abafcc2f755a0ae63a9f0c57
theludovyc <theludovyc@gmail.com>
2023-01-04 15:51:53 +0100

    update TestStartFromLabel

--- Test/TestParser/TestStartFromLabel/TestStartFromLabel.gd
+++ Test/TestParser/TestStartFromLabel/TestStartFromLabel.gd
@@ -1,18 +1,30 @@
 extends GutTest
 
-const file_path = "res://Test/TestParser/TestStartFromLabel/TestStartFromLabel.rk"
+const file_name = "res://Test/TestParser/TestStartFromLabel/TestStartFromLabel.rk"
 
-var say_char:Dictionary
-var say_text:String
-func _on_say(character:Dictionary, text:String):
-	say_char = character
-	say_text = text
+var file_base_name = file_name.get_file().get_basename()
 
 func test_start_from_label():
-	Rakugo.connect("say", self, "_on_say")
+	watch_signals(Rakugo)
 	
-	Rakugo.parse_and_execute_script(file_path, "pictures")
+	Rakugo.parse_and_execute_script(file_name, "pictures")
+
+	yield(yield_to(Rakugo, "execute_script_start", 0.2), YIELD)
 	
 	yield(yield_to(Rakugo, "say", 0.2), YIELD)
-	
-	assert_eq(say_text, "Pictures of places that I have visited.")
+
+	assert_signal_emitted_with_parameters(
+		Rakugo,
+		"say",
+		[{}, "Pictures of places that I have visited."])
+
+	assert_true(Rakugo.waiting_step)
+
+	Rakugo.do_step()
+
+	yield(yield_to(Rakugo, "execute_script_finished", 0.2), YIELD)
+
+	assert_signal_emitted_with_parameters(
+		Rakugo,
+		"execute_script_finished",
+		[file_base_name])
