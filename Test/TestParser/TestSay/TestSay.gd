c103fc2075eecab0fb38b0c8a2e61aabc6b0fa9f
theludovyc <theludovyc@gmail.com>
2023-09-30 00:19:01 +0200

    fix #49 handle espace characters - rem method remove_double_quotes from Executer. - add method treat_string to Parser. - do not directly save result (RegExMatch) in Parser, but create dictionary with them (not for all cases).

--- Test/TestParser/TestSay/TestSay.gd
+++ Test/TestParser/TestSay/TestSay.gd
@@ -26,7 +26,7 @@ func test_say(params=use_parameters(test_params)):
 
 	var result = parsed_array[0][1]
 	
-	assert_eq(result.get_string("text"), "\"Hello, world !\"")
+	assert_eq(result["text"], "Hello, world !")
 
 func test_say_character():
 	var rk_script = [
@@ -47,9 +47,9 @@ func test_say_character():
 
 	var result = parsed_array[0][1]
 
-	assert_eq(result.get_string("character_tag"), "sy")
+	assert_eq(result["character_tag"], "sy")
 	
-	assert_eq(result.get_string("text"), "\"Hello !\"")
+	assert_eq(result["text"], "Hello !")
 
 func test_say_variable():
 	var rk_script = [
@@ -70,4 +70,30 @@ func test_say_variable():
 
 	var result = parsed_array[0][1]
 	
-	assert_eq(result.get_string("text"), "\"My name is <sy.name>\"")
+	assert_eq(result["text"], "My name is <sy.name>")
+	
+func test_say_escape_characters():
+	var rk_script = [
+		"\"first\\nsecond\\n\\nthird\"",
+		"\"\\ttabulation\\t\\ttabulations\""
+	]
+
+	var parsed_script = parser.parse_script(rk_script)
+
+	assert_false(parsed_script.is_empty())
+
+	var parsed_array = parsed_script["parse_array"]
+
+	assert_false(parsed_array.is_empty())
+	
+	assert_true(parsed_script.get("labels", [""]).is_empty())
+
+	assert_true(parsed_array[0][0] == "SAY")
+
+	var result = parsed_array[0][1]
+	
+	assert_eq(result["text"], "first\nsecond\n\nthird")
+	
+	result = parsed_array[1][1]
+	
+	assert_eq(result["text"], "\ttabulation\t\ttabulations")
