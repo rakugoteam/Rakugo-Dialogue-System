5517046cfb8283baa31023fb8ef9fc309d42a160
Jeremi Biernacki <jeremi360@gmail.com>
2023-10-11 08:40:42 +0200

    clean up in plugin.gd and not needed .git conf files inside addons/Rakugo

--- addons/Rakugo/plugin.gd
+++ addons/Rakugo/plugin.gd
@@ -1,6 +1,12 @@
 @tool
 extends EditorPlugin
 
+var editor_interface : EditorInterface
+var script_editor : ScriptEditor
+var code_highlighter : EditorSyntaxHighlighter
+var default_script_types : PackedStringArray
+var editor_settings : EditorSettings
+var default_text_extensions : String
 
 func _enter_tree():
 	# Initialization of the plugin goes here
@@ -12,8 +18,18 @@ func _enter_tree():
 	ProjectSettings.set_setting("addons/rakugo/debug", false)
 	ProjectSettings.set_setting("addons/rakugo/save_folder", "user://saves")
 
-	print("Rakugo is enabled")
+	default_script_types = ProjectSettings.get_setting(
+		"editor/script/search_in_file_extensions")
+	
+	if !("rk" in default_script_types):
+		var script_types = default_script_types.duplicate()
+		script_types.append("rk")
+		ProjectSettings.set_setting(
+			"editor/script/search_in_file_extensions",
+			script_types
+		)
 
+	print("Rakugo is enabled")
 
 func _exit_tree():
 	ProjectSettings.set_setting("addons/rakugo/game_version", null)
@@ -21,3 +37,8 @@ func _exit_tree():
 	ProjectSettings.set_setting("addons/rakugo/narrator/name", null)
 	ProjectSettings.set_setting("addons/rakugo/debug", null)
 	ProjectSettings.set_setting("addons/rakugo/save_folder", null)
+
+	ProjectSettings.set_setting(
+		"editor/script/search_in_file_extensions",
+		default_script_types
+	)
