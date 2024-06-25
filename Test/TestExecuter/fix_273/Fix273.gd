7c380727a2db72db126604ea4ccf90c64d224127
Jeremi Biernacki <jeremi360@gmail.com>
2024-06-25 14:06:22 +0200

    turn fix273 into gut

--- Test/TestExecuter/fix_273/Fix273.gd
+++ Test/TestExecuter/fix_273/Fix273.gd
@@ -1,10 +1,19 @@
-extends RakuScriptDialogue
+extends "res://Test/RakugoTest.gd"
 
-func _ready():
-	Rakugo.sg_say.connect(_on_say)
-	super._ready()
+const file_path = "res://Test/TestExecuter/fix_273/fix273.rk"
 
-func _on_say(character: Dictionary, text: String):
-	print(character.name, " : ", text)
-	Rakugo.step()
+var file_base_name = get_file_base_name(file_path)
+
+func test_273_fix():
+	watch_rakugo_signals()
+
+	await wait_parse_and_execute_script(file_path)
+
+	await assert_variable("test_var", TYPE_INT, 0)
+
+	await wait_say({}, "273 fixed")
+
+	assert_do_step()
+
+	await wait_execute_script_finished(file_base_name)
 
